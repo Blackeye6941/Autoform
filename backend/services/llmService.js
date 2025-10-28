8065a884fb1a3071bbbb79af70e031912a96ecb2
Blackeye6941 <leok37316@gmail.com>
2025-10-28 17:07:31 +0000

    Minor changes

--- backend/services/llmService.js
+++ backend/services/llmService.js
@@ -15,7 +15,7 @@ if(!ai){
 	const uprompt = "Create a form for collecting feedback from students after completing a programming course. Include sections for personal info, course feedback, and instructor evaluation. Add jump logic to skip advanced feedback if the student didn’t complete advanced modules";
 	const res = await ai.models.generateContent({
 		model:"gemini-2.5-flash",
-		contents:`Create a Google Form in structured JSON format based on the following description:${uprompt}.The form should includee:Multiple sections (each with a title and description) and Optional jump logic (go to specific section based on answer) based on user prompt.`,
+		contents:`Create a input for Google Form api in structured JSON format based on the following description:${uprompt}.The form should includee:Multiple sections (each with a title and description) and Optional jump logic (go to specific section based on answer) based on user prompt. The JSON should strictly follow the response schema provided. Dont provide any additional fields out of context and the response schema should be strictly followed.`,
 		config: {
       			responseMimeType: "application/json",
       			responseSchema: formSchema, 
